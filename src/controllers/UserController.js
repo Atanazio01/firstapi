2ecc66d7cee810b17787936eceba99f347555c99
Atanazio01 <marcosd.atanazio@gmail.com>
2021-10-11 16:44:46 -0300

    Receive parameters in the URL

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,7 +5,7 @@ module.exports = {
     const { order } = request.query;
 
     const sortedUsers = users.sort((a, b) => {
-      if(order === 'desc') {
+      if (order === "desc") {
         return a.id < b.id ? 1 : -1;
       }
 
@@ -13,5 +13,19 @@ module.exports = {
     });
     response.writeHead(200, { "Content-Type": "application/json" });
     response.end(JSON.stringify(sortedUsers));
+  },
+
+  getUserById(request, response) {
+    const { id } = request.params;
+
+    const user = users.find((user) => user.id === Number(id));
+
+    if (!user) {
+      response.writeHead(400, { "Content-Type": "application/json" });
+      response.end(JSON.stringify({ error: 'User not found' }));
+    }else {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(user));
+    }
   },
 };
